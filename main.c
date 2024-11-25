53794ae69412286bfb79ca13a907886046d55fe6
Lxc <128301314+Lxcyfxr@users.noreply.github.com>
2024-11-25 12:29:46 +0100

    struct Student created
    student struct with helper sstruct date configured

--- main.c
+++ main.c
@@ -2,3 +2,22 @@
 
 #include <stdio.h>
 
+int main(){
+  typedef struct Date{
+    int year,month,day;
+  } date;
+
+  struct Student{
+    char firstname[100];
+    char lastname[100];
+    char degreeProgram[100];
+    int matrikelnumber;
+    date startdate;
+    date enddate;
+    date birthdate;
+  }
+
+
+
+ return 0;
+  }
